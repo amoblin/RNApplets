035531133843f6cc9a04c5348f968e892ed23920
amoblin@mbp <amoblin@gmail.com>
2016-11-10 18:16:37 +0800

    add module_name configuration

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 {
   "data": [{
     "name": "Hello, World",
-    "uri": "https://github.com/amoblin/RNApplets/raw/master/Apps/HelloWorld.jsbundle"
+    "uri": "https://github.com/amoblin/RNApplets/raw/master/Apps/HelloWorld.jsbundle",
+    "module_name": "HelloWorld"
   },{
     "name": "知乎日报",
-    "uri": "https://github.com/amoblin/RNApplets/raw/master/Apps/ZhiHuDaily.jsbundle"
+    "uri": "https://github.com/amoblin/RNApplets/raw/master/Apps/ZhiHuDaily.jsbundle",
+    "module_name": "ZhiHuDaily"
   }]
 }
